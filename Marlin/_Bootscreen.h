acc1d74b0c4b015fa054686ef297b3cdf515d506
Tiago Jesus <trajesus@hotmail.com>
2023-04-30 22:03:53 +0100

    Update _Bootscreen.h

--- Marlin/_Bootscreen.h
+++ Marlin/_Bootscreen.h
@@ -1,9 +1,31 @@
 /**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2022 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
  * Made with Marlin Bitmap Converter
  * http://marlinfw.org/tools/u8glib/converter.html
  *
- * This bitmap from the file 'fenix_crop.png'
  */
+
 #define CUSTOM_BOOTSCREEN_BMPWIDTH  128
 #define CUSTOM_BOOTSCREEN_BMPHEIGHT 64
 const unsigned char custom_start_bmp[] PROGMEM = {
