932f1012c21da81f63953d37f53eedd8b1ceb95b
Tiago Jesus <trajesus@hotmail.com>
2023-05-19 22:59:11 +0100

    small updates

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2845,13 +2845,13 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC_CONFIG(X)
-    #define X_CURRENT       400        // (mA) RMS current. Multiply by 1.414 for peak current.
-    #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
-    #define X_MICROSTEPS     32        // 0..256
-    #define X_RSENSE          0.11     // Multiplied x1000 for TMC26X
-    #define X_CHAIN_POS      -1        // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ...
-    //#define X_INTERPOLATE  true      // Enable to override 'INTERPOLATE' for the X axis
-    //#define X_HOLD_MULTIPLIER 0.5    // Enable to override 'HOLD_MULTIPLIER' for the X axis
+    #define X_CURRENT        400        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT_HOME   X_CURRENT  // (mA) RMS current for sensorless homing
+    #define X_MICROSTEPS     32         // 0..256
+    #define X_RSENSE         0.11       // Multiplied x1000 for TMC26X
+    #define X_CHAIN_POS      -1         // -1..0: Not chained. 1: MCU MOSI connected. 2: Next in chain, ...
+    //#define X_INTERPOLATE  true       // Enable to override 'INTERPOLATE' for the X axis
+    //#define X_HOLD_MULTIPLIER 0.5     // Enable to override 'HOLD_MULTIPLIER' for the X axis
   #endif
 
   #if AXIS_IS_TMC_CONFIG(X2)
@@ -2865,10 +2865,10 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Y)
-    #define Y_CURRENT       400
-    #define Y_CURRENT_HOME  Y_CURRENT
+    #define Y_CURRENT        400
+    #define Y_CURRENT_HOME   Y_CURRENT
     #define Y_MICROSTEPS     32
-    #define Y_RSENSE          0.11
+    #define Y_RSENSE         0.11
     #define Y_CHAIN_POS      -1
     //#define Y_INTERPOLATE  true
     //#define Y_HOLD_MULTIPLIER 0.5
@@ -2885,10 +2885,10 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Z)
-    #define Z_CURRENT       700
-    #define Z_CURRENT_HOME  Z_CURRENT
+    #define Z_CURRENT        700
+    #define Z_CURRENT_HOME   Z_CURRENT
     #define Z_MICROSTEPS     32
-    #define Z_RSENSE          0.11
+    #define Z_RSENSE         0.11
     #define Z_CHAIN_POS      -1
     //#define Z_INTERPOLATE  true
     //#define Z_HOLD_MULTIPLIER 0.5
@@ -2985,9 +2985,9 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(E0)
-    #define E0_CURRENT      400
+    #define E0_CURRENT       400
     #define E0_MICROSTEPS    32
-    #define E0_RSENSE         0.11
+    #define E0_RSENSE        0.11
     #define E0_CHAIN_POS     -1
     //#define E0_INTERPOLATE true
     //#define E0_HOLD_MULTIPLIER 0.5
